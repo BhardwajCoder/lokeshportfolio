a162102bb9e0769d6660908181a1aaa6a2b20c19
BhardwajCoder <134699651+BhardwajCoder@users.noreply.github.com>
2024-05-31 09:35:49 +0530

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -15,8 +15,8 @@ const Footer = () => {
             <ul >
               <li><a href="#home">Home</a></li>
               <li><a href="#about">About</a></li>
-              <li><a href="#portfolio">Portfolio</a></li>
-              <li><a href="#blog">Blog</a></li>
+              <li><a href="#projects">projects</a></li>
+              <li><a href="#skills">Skills</a></li>
               <li><a href="#contact">Contact</a></li>
             </ul>
           </div>
@@ -28,7 +28,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="footer-bottom">
-          <p>&copy; 2024 Your Name. All rights reserved.</p>
+          <p>&copy; 2024. All rights reserved.</p>
         </div>
       </footer>
       </>
